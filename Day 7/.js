a70ff91c6fc24ed436211c0ee913f809296ae76f
whosatyamraj <satyamrajsingh0811@gmail.com>
2025-09-17 09:56:37 +0530

    Fixed Day 7 folder, removed embedded git repo

---
+++ Day 7/.js
@@ -0,0 +1,34 @@
+// }
+// console.log(a);
+
+
+
+//using function
+// var a = 13;
+// console.log(a);
+// function fun(){
+
+//     a=43;
+//     console.log(a);
+// }
+// fun();
+// console.log(a);
+
+
+// function output(){
+//      var x=y=8;
+// }
+// output();
+// console.log(typeof x);
+// console.log(typeof y);
+
+
+//datatyes in javascript
+//primitive(boolean ,number ,null ,),non primitive(array , string,)
+
+
+// let a="235"-23+23+"true"-false+23-235+235;
+// console.log(a);
+
+// output
+// NaN (not a number)
